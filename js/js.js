7ef3d1871ff701f9dd1365fd33f702fc4042a7c4
seblundin <73648054+seblundin@users.noreply.github.com>
2021-04-22 22:35:09 +0300

    Bugfixes and slight edits

--- js/js.js
+++ js/js.js
@@ -1,27 +1,28 @@
-const navChildren = document.getElementById('nav').children;
+const navElement = document.getElementById('nav');
+const navChildren = navElement.children;
 const palikat = document.getElementsByClassName('palikka');
 const ham = document.getElementById('ham');
 
-function showburger() {
-  ham.removeEventListener('click', showburger);
-  document.getElementById('nav').style.display = 'block';
-  ham.addEventListener('click', hideburger);
-}
 
-function hideburger() {
-  ham.removeEventListener('click', hideburger);
-  document.getElementById('nav').style.display = 'none';
-  ham.addEventListener('click', showburger);
-}
+/*Annetaan mobiilinäkymän navigointipalkin checkboxille tapahtumankuuntelija,
+* joka määrittää näkyykö valikon sisältöä vai ei.*/
+ham.addEventListener('click', function() {
+  if (ham.checked === false) {
+    navElement.style.display = 'block';
+  } else {
+    navElement.style.display = 'none';
+  }
+});
 
-ham.addEventListener('click', showburger);
-
-
+/*Lisätään tapahtumankuuntelija jokaiselle navigointipalkin li elementille*/
 for (let navChild of navChildren) {
   navChild.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    /*Piilotetaan kaikki sivuston div elementit joissa on pääsisältö.*/
     for (let palikatElement of palikat) {
       palikatElement.style.display = 'none';
     }
+    /*Otetaan klikkausta vastaava div elementti esiin.*/
     switch (navChild.id) {
       case 'l_1': document.getElementById('main').style.display = 'block';
       break;
@@ -33,3 +34,14 @@ for (let navChild of navChildren) {
     }
   });
 }
+
+/*Varmistetaan, että navigointipalkin elementit pysyvät näkyvissä mobiilinäkymästä poistuttaessa.*/
+window.addEventListener("resize", function(evt) {
+  evt.preventDefault();
+  //Jos ikkunan koko on suurempi kuin 600px ja naviogintipalkkia ei ole näkyvissä, otetaan se näkyviin.
+  if (document.documentElement.clientWidth > 600 && navElement.style.display === 'none') {
+    navElement.style.display = 'block';
+    //Resetoidaan checkboxin arvo.
+    ham.checked = false;
+  }
+});
