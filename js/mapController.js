65df33afed0028a46ed7aa16876e1017152ccf00
seblundin <73648054+seblundin@users.noreply.github.com>
2021-05-04 13:10:59 +0300

    Refactored mapcontroller

--- js/mapController.js
+++ js/mapController.js
@@ -10,17 +10,18 @@ const allStationsGroup = L.layerGroup().addTo(map);
 //viittaus nappiin
 const nappi = document.getElementById('hakunappi');
 const hakuelementti = document.getElementById('haku');
-nappi.addEventListener('click', haeTiedot);
 
-/*Reittihakuvalintaan kartan asettavan radiobuttonin event listener*/
+/*Reittihakuvalintaan kartan asettavan napin event listener*/
 document.getElementById('reitti').addEventListener('click', event => {
       event.preventDefault();
       allStationsGroup.clearLayers();
       hakuelementti.style.display = 'block';
     },
 );
+/*Hakukentän nappi, joka käynnistää reittihaun.*/
+nappi.addEventListener('click', planRoute);
 
-/*Vuokrapyörälistaukseen kartan asettavan radiobuttonin event listener*/
+/*Vuokrapyörälistaukseen kartan asettavan napin event listener*/
 document.getElementById('kaikki').addEventListener('click', event => {
       event.preventDefault();
       routeGroup.clearLayers();
@@ -29,15 +30,18 @@ document.getElementById('kaikki').addEventListener('click', event => {
     },
 );
 
+/*Muuttujat käyttäjän sijainnille*/
 let latitude = null;
 let longitude = null;
 
+/*funktio joka hakee käyttäjän sijainnin geolocation.getCurrentPosition avulla*/
 function locateUser() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
   }
 }
 
+/*Jos sijainnin haku onnistuu, siirrytään käyttäjän sijaintiin ja kirjataan se ylös.*/
 function success(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
@@ -45,6 +49,7 @@ function success(position) {
   //L.marker([latitude, longitude]).addTo(map).bindPopup('<b>Olet tässä<b/>');
 }
 
+/*Jos sijainnin haku ei onnistu pyydetään käyttäjää laittamaan sijainti päälle, jos sijaintidataa ei vielä ole saatavilla.*/
 function error(error) {
   console.log(error);
   if (latitude === null || longitude === null) {
@@ -53,7 +58,7 @@ function error(error) {
   }
 }
 
-/*graphql request for stop data*/
+/*Haetaan hsl apista kaikki vuokrapyöräpysäkit pääkaupunkiseudulta. Jokainen pysäkki saa oman markerin, jossa on pysäkistä tietoa.*/
 function allBikes() {
   fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
     method: 'POST',
@@ -84,31 +89,35 @@ function allBikes() {
   //L.marker([60.22, 24.92846]).addTo(map);
 }
 
-function haeTiedot(evt) {
+
+/*Reittihakufunktio, joka piirtää kartalle reitin käyttäjän sijainnista haettuun määränpäähän vuokrapyöräpysäkkien kautta.*/
+function planRoute(evt) {
   evt.preventDefault();
   locateUser();
 
-  //haetaan paikkakunta elementistä
-  const paikkakunta = document.getElementById('paikkakunta').value;
-  console.log(paikkakunta);
+  //haetaan hakukohde elementistä
+  const searchData = document.getElementById('paikkakunta').value;
+  console.log(searchData);
 
   const apikey = 'bda75ef0309aec2639f2d2b9f73d35ab';
   //const apikey="6d8eb626a8e105e3350a67b724ae2e1a";
 
   const uri3 = 'https://api.openweathermap.org/data/2.5/weather?q=' +
-      paikkakunta + '&appid=' + apikey;
-  const uri4 = 'https://nominatim.openstreetmap.org/?addressdetails=1&q=' + paikkakunta + '&format=json&limit=1'
+      searchData + '&appid=' + apikey;
+  const uri4 = 'https://nominatim.openstreetmap.org/?addressdetails=1&q=' + searchData + '&format=json&limit=1'
 
+  /*Hakukohteen koordinaattien muuttujat*/
   let targetLat = null;
   let targetLon = null;
 
+  /*apipyyntö openstreetmapille hakukohde sisältönään*/
   fetch(uri4)
-      //kun saadaan vastaus kutsutaan json metodia mikä tekee siitä javascriptin tietorakenteen
       .then(vastaus => vastaus.json())
-      .then(json => naytaTulos2(json))
+      .then(json => plotPath(json))
       .catch(error => console.log(error));
 
-  function naytaTulos2(json) {
+  /*Funktio, joka hakee hsl:ltä reittidatan käyttäjän ja määränpään koordinaattien välille ja piirtää sen avulla reitin karttaan*/
+  function plotPath(json) {
     console.log(json);
 
     /*Asetetaan koordinaatit tavoitesijainnille*/
@@ -117,11 +126,12 @@ function haeTiedot(evt) {
 
     /*Resetoidaan reittimerkinnät*/
     routeGroup.clearLayers();
-    console.log(targetLat);
+    /*console.log(targetLat);
     console.log(targetLon);
     console.log(latitude);
-    console.log(longitude);
-    /*Apipyyntö */
+    console.log(longitude);*/
+
+    /*Apipyyntö hsl:lle. Sisältää paljon dataa jota ei lopuksi kuitenkaan käytetä.*/
     fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -172,19 +182,24 @@ function haeTiedot(evt) {
     }`,
       }),
     }).then(res => res.json()).then(res => {
+      /*Jos hsl apilta tulee takaisin reitti, lähdetään sitä käsittelemään.*/
       if (!(res.data.plan.itineraries.length === 0)) {
         console.log(res);
+        /*Jokaiselle saadulle etapille tehdään toimenpiteet.*/
         res.data.plan.itineraries[0].legs.forEach(leg => {
           console.log(leg);
+          /*Kirjataan etapista dataa ylös markeria varten.*/
           let content = new Date(leg.startTime).toLocaleTimeString() +
               ' - ' + new Date(leg.endTime).toLocaleTimeString() + ':<br/>';
           content += leg.from.name;
           content += ' -> ';
           content += leg.to.name;
 
+          /*Luodaan markerit etapin alkupisteelle ja määränpäälle.*/
           let marker1 = L.marker([leg.from.lat, leg.from.lon]);
           let marker2 = L.marker([leg.to.lat, leg.to.lon]);
 
+          /*Tehdään tietynlainen sisältö markerille riippuen siitä, onko se pyöräasema vai ei.*/
           if (!(leg.from.bikeRentalStation === null)) {
             marker1.
                 bindPopup(
@@ -208,6 +223,7 @@ function haeTiedot(evt) {
                 addTo(routeGroup);
           }
 
+          /*Piirretään viiva etapille saadun polyline datan avulla.*/
           let leg_polyline = L.polyline([],
               {
                 color: '#' + Math.floor(Math.random() * 16777215).toString(16),
@@ -219,6 +235,7 @@ function haeTiedot(evt) {
             leg_polyline.addLatLng(L.latLng(points[i][0], points[i][1]));
           }
         });
+        /*Jos hsl api ei osaa tehdä reittiä koordinaattipisteiden välille, ilmoitetaan siitä käyttäjälle.*/
       } else {
         alert('HSL ei löytänyt reittiä.');
       }
